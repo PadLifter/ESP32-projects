9b769e67bf2e18965c31565876639e18c7ee5036
PadLifter <lentodrone@gmail.com>
2023-01-18 14:40:42 +0200

    OLED reads pot value

--- OLED gauge/OLED gauge.ino
+++ OLED gauge/OLED gauge.ino
@@ -7,65 +7,111 @@
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
 
 // Define a struct
-struct pinRead 
-{
-  int pin;  // analog channel indentifier
-  int value; // sensor output value
+struct pinRead {
+  int pin;    // analog channel indentifier
+  int value;  // sensor output value
 };
 
 QueueHandle_t structQueue;
 
 void setup() {
-Serial.begin(115200);
+  Serial.begin(115200);
 
-  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
+  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
     Serial.println(F("SSD1306 allocation failed"));
   }
   // Clear the buffer.
-   display.clearDisplay();
+  display.clearDisplay();
 
 
-// create Structure Queue
+  // create Structure Queue
   structQueue = xQueueCreate(10, sizeof(struct pinRead));
 
   if (structQueue != NULL) {
 
-  xTaskCreatePinnedToCore(
-    displayTask,      // Function that should be called
-    "displayTask",    // Name of the task (for debugging)
-    2048,               // Stack size (bytes)
-    NULL,               // Parameter to pass
-    1,                  // Task priority
-    NULL,               // Task handle
-    1          // Core you want to run the task on (0 or 1)
-);
+    xTaskCreatePinnedToCore(
+      displayTask,    // Function that should be called
+      "displayTask",  // Name of the task (for debugging)
+      2048,           // Stack size (bytes)
+      NULL,           // Parameter to pass
+      1,              // Task priority
+      NULL,           // Task handle
+      1               // Core you want to run the task on (0 or 1)
+    );
   }
 }
 
 void loop() {
   uint adc = analogRead(ADC_POT);
- Serial.println (adc);
- delay(500);
+  Serial.println(adc);
+  delay(500);
 
- // push pot value to queue
- struct pinRead currentPinRead;  // define a structure of type pinRead 
-    currentPinRead.pin = 0; // assign value '0' to pin element of struct
-    currentPinRead.value = adc; // Read adc value 
-    xQueueSend(structQueue, &currentPinRead, portMAX_DELAY); //write struct message to queue
-
+  // push pot value to queue
+  struct pinRead currentPinRead;                                  // define a structure of type pinRead
+  currentPinRead.pin = 0;                                         // assign value '0' to pin element of struct
+  currentPinRead.value = adc;                                     // Read adc value
+  xQueueSendToBack(structQueue, &currentPinRead, portMAX_DELAY);  //write struct message to queue
 }
 
-void displayTask(void*parameter){
-  for(;;){
-    Serial.println ("Display");
-    delay (1000);
-    // Display Text
-  display.setTextSize(1);
-  display.setTextColor(WHITE);
-  display.setCursor(0, 28);
-  display.println("Hello!!!!!!");
-  display.display();
+void displayTask(void* parameter) {
+  for (;;) {
 
+    int POT = 0;                    // temporary variable to hold pot value
+    int TERM = 0;                   // temporary variable to hold terminal input value
+    struct pinRead currentPinRead;  // structure to hold receiv data
+    
+    // Read structure elements from queue and check if data received successfully
+    if (xQueueReceive(structQueue, &currentPinRead, portMAX_DELAY) == pdPASS) {
+      // print received data elements on serial montor
+      Serial.print("Pin: ");
+      Serial.print(currentPinRead.pin);
+      Serial.print(" Value: ");
+      Serial.println(currentPinRead.value);
+
+      // If condition checks,  if data receive from channel zero
+      // If yes, store sensor value member of structure in temporary pot variable
+      // Print pot value to oled
+      if (currentPinRead.pin == 0) {
+        POT = currentPinRead.value;
+
+        // Clear row
+        display.fillRect(0, 0, 128, 14, BLACK);
+
+        // Print pot value
+        display.setTextSize(2);
+        display.setTextColor(WHITE);
+        display.setCursor(0, 0);
+        display.println(POT);
+        BarGraph(POT, currentPinRead.pin);
+        display.display();
+      }
+
+      // If condition checks,  if data receive from channel one (A1)
+      // If yes, store sensor value member of structure in temporary LDR variable
+      if (currentPinRead.pin == 1) {
+
+        TERM = currentPinRead.value;
+       
+    
+
+        Serial.println("Display");
+        delay(1000);
+ 
+      }
+     
+    }
   }
-  vTaskDelete(NULL);
+   vTaskDelete(NULL);
+}
+
+void BarGraph (uint val, int pos){
+  uint w = val * (127/4095);
+ if (pos == 0){
+  display.fillRect(0, 17, 127, 14, BLACK);
+  display.drawRect(0, 17, 127, 14, WHITE);
+  display.fillRect(0, 17, w, 14, WHITE);
+ }
+
+
+
 }

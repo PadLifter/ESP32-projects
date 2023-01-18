891d5acb63a51941b6b12730e393eee39aa29766
PadLifter <lentodrone@gmail.com>
2023-01-18 13:01:47 +0200

    Pot works

--- OLED gauge/OLED gauge.ino
+++ OLED gauge/OLED gauge.ino
@@ -1,8 +1,7 @@
-#include <Arduino_FreeRTOS.h>
-#include <queue.h>
+#define ADC_POT A0
 
 void setup() {
-Serial.begin(112500)
+Serial.begin(115200);
 
   xTaskCreatePinnedToCore(
     displayTask,      // Function that should be called
@@ -11,12 +10,23 @@ Serial.begin(112500)
     NULL,               // Parameter to pass
     1,                  // Task priority
     NULL,               // Task handle
-    1,          // Core you want to run the task on (0 or 1)
+    1          // Core you want to run the task on (0 or 1)
 );
 
 }
 
 void loop() {
-  
+  uint adc = analogRead(ADC_POT);
+ Serial.println (adc);
+ delay(500);
 
+}
+
+void displayTask(void*parameter){
+  for(;;){
+    Serial.println ("Display");
+    delay (1000);
+
+  }
+  vTaskDelete(NULL);
 }

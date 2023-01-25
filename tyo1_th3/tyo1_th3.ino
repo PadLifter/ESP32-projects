0236258009dd0a8c5231c52b27a439e020f2eff6
PadLifter <lentodrone@gmail.com>
2023-01-25 13:41:09 +0200

    Mesh working

--- tyo1_th3/tyo1_th3.ino
+++ tyo1_th3/tyo1_th3.ino
@@ -6,6 +6,14 @@
 #define   MESH_PASSWORD   "sanasala"
 #define   MESH_PORT       5555
 
+// GPIO where LED is connected to
+
+const int fanPin = 17;  //A3, Fan control
+const int potPin = 34;  //A2, pot
+uint adc = 0;
+uint msg_val = 0;
+
+
 Scheduler userScheduler; // to control your personal task
 painlessMesh  mesh;
 
@@ -15,15 +23,14 @@ void sendMessage() ; // Prototype so PlatformIO doesn't complain
 Task taskSendMessage( TASK_SECOND * 1 , TASK_FOREVER, &sendMessage );
 
 void sendMessage() {
-  String msg = "Hi from Feather! :)";
-  msg += mesh.getNodeId();
-  mesh.sendBroadcast( msg );
-  taskSendMessage.setInterval( random( TASK_SECOND * 1, TASK_SECOND * 5 ));
+mesh.sendBroadcast((String)adc);
+taskSendMessage.setInterval(TASK_SECOND * 1);
+
 }
 
 // Needed for painless library
 void receivedCallback( uint32_t from, String &msg ) {
-  Serial.printf("startHere: Received from %u msg=%s\n", from, msg.c_str());
+ msg_val = msg.toInt();
 }
 
 void newConnectionCallback(uint32_t nodeId) {
@@ -38,37 +45,12 @@ void nodeTimeAdjustedCallback(int32_t offset) {
     Serial.printf("Adjusted time %u. Offset = %d\n", mesh.getNodeTime(),offset);
 }
 
-// GPIO where LED is connected to
-const int ledPin = 25;  //LED output
-const int fanPin = 21;  //Fan control
-const int potPin = 26;  //A0, pot
-const int freq = 5000;
-int potValue = 0;
-
-// setting PWM properties
-const int fanChannel = 0;
-const int resolution = 8;
-
-// Handle received and sent messages
-String message = "";
-char incomingChar;
-int speed = 0;
-
-// Timer: auxiliar variables
-unsigned long previousMillis = 0;  // Stores last time temperature was published
-const long interval = 3000;        // interval at which to publish sensor readings
-
 
 
 void setup() {
-  pinMode(ledPin, OUTPUT);
   pinMode(fanPin, OUTPUT);
   Serial.begin(115200);
 
-  // configure fan PWM functionalitites
-  ledcSetup(fanChannel, freq, resolution);
-  // attach the channel to the GPIO to be controlled
-  ledcAttachPin(fanPin, fanChannel);
 
 //mesh.setDebugMsgTypes( ERROR | MESH_STATUS | CONNECTION | SYNC | COMMUNICATION | GENERAL | MSG_TYPES | REMOTE ); // all types on
   mesh.setDebugMsgTypes( ERROR | STARTUP );  // set before init() so that you can see startup messages
@@ -84,13 +66,16 @@ void setup() {
 }
 
 void loop() {
-  unsigned long currentMillis = millis();
-
-  // Send pot readings
   
+  adc = analogRead(potPin);
+  // Send pot readings
+if (msg_val > 1000 || adc > 1000)
+digitalWrite(fanPin, HIGH);
+else
+digitalWrite(fanPin, LOW);
   // it will run the user scheduler as well
   mesh.update();
-    potValue = analogRead(potPin);
+  
   
 
   
@@ -111,6 +96,4 @@ void loop() {
     ledcWrite(fanChannel, speed);
   }
 */
-
-  delay(20);
 } 

4bd6a791e97ceaf657175f7d49399c1ff5148b4f
PadLifter <lentodrone@gmail.com>
2023-01-25 14:48:57 +0200

    Timer function working

--- Timers/Timers.ino
+++ Timers/Timers.ino
@@ -1,9 +1,88 @@
-void setup() {
-  // put your setup code here, to run once:
 
+#define LED1  12
+#define LED2  13
+
+
+class AlertLED
+{
+
+TimerHandle_t tmr = nullptr;  // Creating timer handle
+
+int             Led_pin;
+
+unsigned        period_ms;
+
+volatile bool   stat=0;
+
+
+public:
+
+AlertLED(int Led_pin, unsigned period_ms=1000 );
+
+void Tstart();
+
+static void callback(TimerHandle_t tmr);
+  
+};
+
+
+AlertLED::AlertLED(int Led_pin, unsigned period_ms )
+{ 
+  this ->Led_pin=Led_pin;
+  this ->period_ms =period_ms;
+
+  pinMode(this->Led_pin,OUTPUT);
+  digitalWrite(this->Led_pin,LOW);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void AlertLED::Tstart()
+{
 
+    tmr = xTimerCreate
+    (
+      "MyTimer",          // Timer name
+      pdMS_TO_TICKS(100), // Period time
+      pdTRUE,             // Auto reload
+      this,               // Timer ID
+      AlertLED::callback  // callback
+     );
+           
+    if( xTimerStart(tmr, 10 ) != pdPASS ) 
+    {
+     Serial.println("Timer start error");
+    }
+  
 }
+
+void AlertLED::callback(TimerHandle_t tmr)
+{
+
+  AlertLED *obj = (AlertLED*) pvTimerGetTimerID(tmr);
+    digitalWrite(obj->Led_pin,obj->stat);
+
+    obj-> stat ^= true; 
+Serial.print(obj->Led_pin);
+    Serial.println("Tick ");
+}
+
+
+
+static AlertLED alert1(LED1, 300);
+static AlertLED alert2(LED2, 80);
+
+
+void setup()
+{
+
+Serial.begin(115200);
+Serial.println("\n\n");
+Serial.println(" start ");
+
+alert1.Tstart();
+alert2.Tstart();   
+    
+}
+
+
+void loop()
+{}

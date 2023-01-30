a2fb1806240076536d262f5630a9d3e15c3cb859
PadLifter <lentodrone@gmail.com>
2023-01-30 13:18:18 +0200

    Reading voltage works

--- ESP32 oscilloscope/ESP32 oscilloscope.ino
+++ ESP32 oscilloscope/ESP32 oscilloscope.ino
@@ -1,9 +1,17 @@
-void setup() {
-  // put your setup code here, to run once:
+const int measurePin = 34; // GPIO 34 (Analog ADC1_CH6) 
 
+int adcValue = 0; // storing adc value
+
+void setup() {
+  Serial.begin(115200);
+  delay(1000);
+  analogReadResolution(8);
+  analogSetAttenuation(ADC_11db); 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 
+  adcValue = analogRead(measurePin); // Reading ADC value  
+  Serial.println(adcValue);
+  delay(500);
 }

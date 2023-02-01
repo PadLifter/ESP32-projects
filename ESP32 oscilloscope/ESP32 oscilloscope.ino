ce8f108d3d617840ca588e5222d147be5e27d297
PadLifter <lentodrone@gmail.com>
2023-02-01 15:04:25 +0200

    Added Trigger function

--- ESP32 oscilloscope/ESP32 oscilloscope.ino
+++ ESP32 oscilloscope/ESP32 oscilloscope.ino
@@ -1,8 +1,11 @@
 const int measurePin = 4; // GPIO 04 (Analog ADC2_CH0) 
 const int potPin = 36;    // GPIO 36 (Analog ADC1_CH0)
 
-int adcValue = 0;   // Storing adcValue
-int potValue = 0;   // Storing potValue
+uint8_t adcValue = 0;       // Storing adcValue
+uint8_t potValueTrig = 0;       // Storing potValue
+int sampleCount = 0;        // Storing samples
+
+uint8_t sine[100];
 
 void setup() {
   Serial.begin(115200);
@@ -11,13 +14,41 @@ void setup() {
   analogSetAttenuation(ADC_11db); // Sets the input attenuation
 }
 
+void tableWrite(uint8_t adcValue, uint8_t potValueTrig){
+  sine[sampleCount] = adcValue;
+  if(sine[0] - potValueTrig > -2 && sine[0] - potValueTrig < 2){
+    if(sampleCount == 1 && sine[1] <= sine[0]){
+    sampleCount = 0;
+    Serial.print("Voltage decreasing");
+    }
+    else{
+      if(sampleCount == 99){
+      sampleCount = 0;
+      Serial.print("Array completed");
+      }
+      else{sampleCount++;}
+      Serial.print("Samplecount: ");
+      Serial.println(sampleCount);
+    }
+  }
+}
+
 void loop() {
-  potValue = analogRead(potPin); // Reading ADC potValue
+
+  potValueTrig = analogRead(potPin); // Reading ADC potValue
   adcValue = analogRead(measurePin); // Reading ADC voltage value
+  tableWrite(adcValue, potValueTrig);
+
+
+  
+}
+
+/*
+
   Serial.println("Voltage: ");
   Serial.println(adcValue);
   Serial.print('\n');
   Serial.println("potValue: ");
   Serial.println(potValue);
   delay(1000);
-}
+  */
